e44302033442575e8286b5ee2871090b520bd5d2
HukovaNastya <hukovanasta@gmail.com>
2023-05-15 22:44:43 +0300

    added some basic elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import Title from './components/Title';
 import './App.css';
+import './assets/img/UploadImg.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title text="Drag to upload"/>
+      <p className="drag-and-drop-paragraph">
+        You can drag your fi
+        <span className='drag-and-drop-span'>
+        le to a certain area to upload it.
+        </span>
+      </p>
+      <div className='drag-and-drop-wrapper'>
+       
+      </div>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+const Title = ({text}) => {
+  return (
+    <>
+      <h1 className='main-title'>{text}</h1>
+    </>
+  )
+}
+
+export default Title;
