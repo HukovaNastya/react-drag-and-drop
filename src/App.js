312720ff6d515936faeeb9bdd27c50164db61e05
HukovaNastya <hukovanasta@gmail.com>
2023-05-16 23:05:21 +0300

    finished ui for task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Title from './components/Title';
 import './App.css';
 import './assets/img/UploadImg.png';
+// import uploadImg from './assets/img/UploadImg.png';
+import UploadImg from './components/UploadImg';
 
 function App() {
   return (
@@ -13,7 +15,11 @@ function App() {
         </span>
       </p>
       <div className='drag-and-drop-wrapper'>
-       
+        <div className="drop-file-input__label">
+          <UploadImg/>
+          <p>Drag & Drop your files <span>here or Click to browse file</span></p>
+        </div>
+        <input type="file" value="" onChange={() => console.log('Hello word!')}/>
       </div>
     </div>
   );
